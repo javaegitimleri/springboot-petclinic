92ff06afb01471f15ccdcfd0899b94e9042f2ec6
ksevindik <ksevindik@harezmi-asus-550>
2018-03-22 17:00:02 +0300

    created spring boot application class

---
+++ src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
@@ -0,0 +1,11 @@
+package com.javaegitimleri.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+}
