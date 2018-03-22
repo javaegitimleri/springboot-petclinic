8c6af73e4e229903262d8225869df094d6639777
ksevindik <ksevindik@harezmi-asus-550>
2018-03-22 19:41:07 +0300

    created a test servlet

--- src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
+++ src/main/java/com/javaegitimleri/petclinic/PetClinicApplication.java
@@ -3,9 +3,11 @@ package com.javaegitimleri.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value=PetClinicProperties.class)
+@ServletComponentScan
 public class PetClinicApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PetClinicApplication.class, args);
