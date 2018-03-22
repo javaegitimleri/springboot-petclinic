576866247d1958c8c5e2a32d1f4b74288ec94b9a
ksevindik <ksevindik@harezmi-asus-550>
2018-03-22 19:47:09 +0300

    created a test filter

---
+++ src/main/java/com/javaegitimleri/petclinic/web/TestFilter.java
@@ -0,0 +1,52 @@
+package com.javaegitimleri.petclinic.web;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class TestFilter
+ */
+@WebFilter(urlPatterns = "/TestServlet")
+public class TestFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public TestFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		response.getWriter().write("before...");
+		chain.doFilter(request, response);
+		response.getWriter().write("after...");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
