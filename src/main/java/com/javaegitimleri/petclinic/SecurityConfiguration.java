b9275dff7b31fb24f31e1bc42dcd5204c27f769b
ksevindik <ksevindik@harezmi-asus-550>
2018-03-23 19:07:31 +0300

    customized web security configuration

---
+++ src/main/java/com/javaegitimleri/petclinic/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.javaegitimleri.petclinic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**/favicon.ico", "/css/**", "js/**", "/images/**", "/webjars/**")
+				.permitAll();
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+}
