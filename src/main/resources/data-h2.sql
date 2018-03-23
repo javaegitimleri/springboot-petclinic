a3019cb75fdc01deaed0f03e785423e4522eed50
ksevindik <ksevindik@harezmi-asus-550>
2018-03-23 17:28:27 +0300

    created schema.sql and data.sql scripts to populate db

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,24 @@
+INSERT INTO t_owner (id,first_name,last_name) VALUES (1, 'Ziya', 'Ferit');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (2, 'Beşir', 'Dal');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (3, 'Eda', 'Rize');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (4, 'Hadi', 'Duru');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (5, 'Pınar', 'Mus');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (6, 'Çiğdem', 'Su');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (7, 'Aslı', 'Zor');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (8, 'Murat', 'Eski');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (9, 'Davut', 'Saz');
+INSERT INTO t_owner (id,first_name,last_name) VALUES (10, 'Kadir', 'Mutlu');
+
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (1, 'Maviş', '2005-09-07', 1);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (2, 'Donetello', '2008-08-06', 1);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (3, 'Karabaş', '2001-04-17', 1);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (4, 'Joe', '2009-03-07', 2);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (5, 'Canavar', '2010-11-30', 2);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (6, 'Tatlım', '2010-01-20', 3);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (7, 'Samanta', '2008-09-04', 5);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (8, 'Boncuk', '2008-09-04', 5);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (9, 'Şanslı', '2007-08-06', 5);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (10, 'Karaburun', '2009-02-24', 6);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (11, 'Twetty', '2000-03-09', 7);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (12, 'Tarçın', '2000-06-24', 8);
+INSERT INTO t_pet (id,name,birth_date,owner_id) VALUES (13, 'Sarı', '2002-06-08', 9);
