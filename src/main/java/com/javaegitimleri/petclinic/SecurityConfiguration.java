5c9aaaa2746a10f1e560521bd856b7a001c357a3
ksevindik <ksevindik@harezmi-asus-550>
2018-03-23 20:05:24 +0300

    added_remember_me_support

--- src/main/java/com/javaegitimleri/petclinic/SecurityConfiguration.java
+++ src/main/java/com/javaegitimleri/petclinic/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package com.javaegitimleri.petclinic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**/favicon.ico", "/css/**", "js/**", "/images/**", "/webjars/**","/login.html")
 				.permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
 		http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").failureUrl("/login.html?loginFailed=true");
+		
+		http.rememberMe().userDetailsService(userDetailsService);
 	}
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,11 +8,12 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>PetClinic Login Page</h1>
+	<h1>PetClinic Login Page</h1>
 	<form action="login" method="post">
 		Username:<input name="username" type="text" /><br /> Password:<input
-			name="password" type="password" /><br /> <input
-			type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}">
+			name="password" type="password" /><br /> <input type="hidden"
+			name="${_csrf.parameterName}" value="${_csrf.token}">
+		Remember Me:<input name="remember-me" type="checkbox"><br />
 		<input type="submit" value="Login">
 	</form>
 	<font color="red"> <c:if test="${not empty param.loginFailed}">
