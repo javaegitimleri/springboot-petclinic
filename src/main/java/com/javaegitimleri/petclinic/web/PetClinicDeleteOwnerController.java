f6154e3286e83094ba66a647341f07694e6baa91
ksevindik <ksevindik@harezmi-asus-550>
2018-07-13 14:15:47 +0300

    implemented owner delete scenario

---
+++ src/main/java/com/javaegitimleri/petclinic/web/PetClinicDeleteOwnerController.java
@@ -0,0 +1,32 @@
+package com.javaegitimleri.petclinic.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaegitimleri.petclinic.model.Owner;
+import com.javaegitimleri.petclinic.service.PetClinicService;
+
+@Controller
+public class PetClinicDeleteOwnerController {
+	
+	@Autowired
+	private PetClinicService petClinicService;
+	
+	@RequestMapping(value="/owners/delete/{id}",method=RequestMethod.GET)
+	public String loadOwner(@PathVariable Long id, ModelMap modelMap) {
+		Owner owner = petClinicService.findOwner(id);
+		modelMap.put("owner", owner);
+		return "deleteOwner";
+	}
+	
+	@RequestMapping(value="/owners/delete/{id}",method=RequestMethod.POST)
+	public String handleFormSubmit(@PathVariable Long id) {
+		petClinicService.deleteOwner(id);
+		return "redirect:/owners";
+	}
+
+}
