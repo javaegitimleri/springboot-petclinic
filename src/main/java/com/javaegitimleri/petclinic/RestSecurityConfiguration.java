6b84e1a0b26972ddc39c28ea0a09e9ce08ae891d
ksevindik <ksevindik@harezmi.com.tr>
2019-04-16 11:45:31 +0300

    rest security config bug fix

--- src/main/java/com/javaegitimleri/petclinic/RestSecurityConfiguration.java
+++ src/main/java/com/javaegitimleri/petclinic/RestSecurityConfiguration.java
@@ -9,7 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 public class RestSecurityConfiguration extends AbstractSecurityConfiguration {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		//super.configure(http);
+		http.antMatcher("/rest/**");
 		http.authorizeRequests().antMatchers("/rest/**").access("hasRole('EDITOR')");
 		http.csrf().disable();
 		http.httpBasic();
